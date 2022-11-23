1632d624fe16a6e77e3e4547497a7368367cbe18
Stephen-Walking <brockvdwest@gmail.com>
2022-11-23 10:41:09 +0200

    Update Entity

--- src/Entity.py
+++ src/Entity.py
@@ -6,10 +6,16 @@ class Entity:
     coords = arr([0,0])
     xvel = 0
     yvel = 0
+    health = 0
 
-    def __init__(self, entityID, hitbox, coords):
+    def __init__(self, entityID, hitbox, coords, health):
         self.entityID = entityID
         self.hitbox = hitbox
         self.coords = coords
+        self.health = health
 
     def move():
+        pass
+
+    def health(healthchange):
+        health += healthchange
