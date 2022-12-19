169f0c70f203815cea2e9cf3e3364475ca8c0937
DojaccR <dojaccr@gmail.com>
2022-12-19 17:24:07 +0200

    sand texture

--- src/testing space.py
+++ src/testing space.py
@@ -2,16 +2,24 @@ import os
 import random as r
 
 map = open("map1.txt", "w")
+mapBlocks = []
+chance = 100
+decay = 0.95
 
 x = ""
 
 for i in range(100):
+    mapBlocks.append([])
     for j in range(100):
-        if int(r.random()*2) == 1:
-            x+="0"
-        else:
-            x+="1"
+        mapBlocks[j].append([0])
 
-    x+="\n"
+startCor = [r.random()*100, r.random()*100]
+
+corList = [startCor]
+
+while len(corList) > 0:
+
 
 map.write(x)
+
+def checkAdjacent(cor, mapSize, mapBlocks):
